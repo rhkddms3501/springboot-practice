a9c605f323370c59bea3a4dcf15c40627bfdcc6e
rhkddms3501 <rhkddms3501@naver.com>
2023-02-16 09:52:45 +0900

    springboot-helloworld

--- springboot-helloworld/src/main/java/com/douzone/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/douzone/springboot/helloworld/HelloWorldApplication.java
@@ -2,6 +2,7 @@ package com.douzone.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 /**
  * 
  * 1. 스프링 애플리케이션의 부트스트래핑 역할 : Bootstraping Class 
@@ -24,7 +25,22 @@ public class HelloWorldApplication {
 		 * 		- 서버 인스턴스 실행
 		 * 5. ApplicationRunner 인터페이스를 구현한 빈을 Application Context에서 찾아서 실행
 		 */
-		SpringApplication.run(HelloWorldApplication.class, args);
+//		ConfigurableApplicationContext ac = null;
+//		try {
+//			ac = SpringApplication.run(HelloWorldApplication.class, args);
+//		} catch (Throwable ex) {
+//			ex.printStackTrace();
+//		}finally {
+//			if(ac != null) {
+//				ac.close();
+//			}
+//		}
+		
+		// 자원정리 ( .close() 를 자동으로 해준다. )
+		// try ~ with ~ resources
+		try( ConfigurableApplicationContext ac = SpringApplication.run(HelloWorldApplication.class, args) ){
+			
+		}
 	}
 
 }
